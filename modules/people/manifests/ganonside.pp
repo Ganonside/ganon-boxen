47f72417f362e1c28898550b9037845276d2f0aa
Jon Horner <hornerjn@mail.uc.edu>
2015-05-19 12:38:20 -0400

    modified ganonside manifest

--- modules/people/manifests/ganonside.pp
+++ modules/people/manifests/ganonside.pp
@@ -1,4 +1,12 @@
 class people::ganonside {
+	include chrome
+	include dash
+	include iterm2::stable
+	include slack
+	include spectacle
+	include sudo
+	include zsh
+
 	include atom
 	atom::package { "atom-beautify": }
 	atom::package { "language-javascript-better": }
@@ -8,6 +16,17 @@ class people::ganonside {
 	atom::theme { "seti-ui": }
 	atom::theme { "seti-syntax": }
 	atom::theme { "ganon-syntax": }
+
+	include osx
+	osx::dock::utohide
+	osx::dock:disable_dashboard
+	osx::finder::show_all_on_desktop
+	osx::finder::show_hidden_files
+	osx::finder::unhide_library
+	osx::finder::show_all_filename_extensions
+	class { 'osx::dock::position':
+		position => 'left'
+	}
 
 	$home = "/Users/${::boxen_user}"
 }
